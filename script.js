cc25be03d11a20279d0efe32a1e2d62dd6cc6ff1
femmytedrey <femmytedrey@gmail.com>
2023-08-20 02:45:24 +0100

    form validation in progress

--- script.js
+++ script.js
@@ -48,51 +48,51 @@ const setHamburger = () => {
 
 setHamburger();
 
-document.addEventListener("DOMContentLoaded", function(){
-  const form = document.getElementById("form");
-  const emailInput = document.getElementById("Email");
-  const emailError = document.querySelector(".error-message");
+const letsValidateEmail = () => {
+  document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("form");
+    const emailInput = document.getElementById("Email");
+    const emailError = document.querySelector(".error-message");
 
-  form.addEventListener("submit", function(event){
-      if(!validateEmail()){
-          event.preventDefault();
-      }
+    form.addEventListener("submit", function (event) {
+        if (!validateEmail()) {
+            event.preventDefault();
+        }
+    });
+
+    emailInput.addEventListener("input", function () {
+        clearErrorMessage();
+    });
+
+    function validateEmail() {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const emailValue = emailInput.value.trim();
+
+        clearErrorMessage();
+
+        if (emailValue === "") {
+            displayErrorMessage("Email cannot be empty.");
+            return false;
+        } else if (!emailRegex.test(emailValue)) {
+            displayErrorMessage("Please insert a valid email!");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function displayErrorMessage(message) {
+        emailError.textContent = message;
+        emailInput.classList.add("error");
+        
+    }
+
+    function clearErrorMessage() {
+        emailError.textContent = "";
+        emailInput.classList.remove("error");
+    }
   });
+}
 
-  emailInput.addEventListener("input", function () {
-      clearErrorMessage();
-  });
+letsValidateEmail()
 
-  function validateEmail(){
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      const emailValue = emailInput.value.trim();
-
-      clearErrorMessage();
-
-      if(emailValue === ""){
-          displayErrorMessage("Email cannot be empty.");
-          return false;
-      }
-      else if(!emailRegex.test(emailValue)){
-          displayErrorMessage("Invalid Email!");
-          return false;
-      }else{
-          return true
-      }
-  }
-  
-  
-
-  function displayErrorMessage(message){
-      emailError.textContent = message;
-      emailInput.classList.add("error")
-
-  }
-
-  function clearErrorMessage(){
-      emailError.textContent = "";
-      emailInput.classList.remove("error");
-  }
-
-
-});
