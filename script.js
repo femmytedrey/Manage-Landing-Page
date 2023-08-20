3cceb72df82e52ad47d83d9930940712b1d0fb74
femmytedrey <femmytedrey@gmail.com>
2023-08-20 01:47:48 +0100

    javascript in progress

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.querySelector('.middle2bottomtop1').addEventListener('scroll', updateAc
 updateActiveCircle();
 
 
-function setHanmburger(){
+const setHamburger = () => {
     const hamburger = document.querySelector('.hamburger');
     const closehamburger = document.querySelector('.closehamburger');
     const menu = document.querySelector('.list1');
@@ -46,5 +46,53 @@ function setHanmburger(){
     });
 }
 
-setHanmburger();
+setHamburger();
 
+document.addEventListener("DOMContentLoaded", function(){
+  const form = document.getElementById("form");
+  const emailInput = document.getElementById("Email");
+  const emailError = document.querySelector(".error-message");
+
+  form.addEventListener("submit", function(event){
+      if(!validateEmail()){
+          event.preventDefault();
+      }
+  });
+
+  emailInput.addEventListener("input", function () {
+      clearErrorMessage();
+  });
+
+  function validateEmail(){
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      const emailValue = emailInput.value.trim();
+
+      clearErrorMessage();
+
+      if(emailValue === ""){
+          displayErrorMessage("Email cannot be empty.");
+          return false;
+      }
+      else if(!emailRegex.test(emailValue)){
+          displayErrorMessage("Invalid Email!");
+          return false;
+      }else{
+          return true
+      }
+  }
+  
+  
+
+  function displayErrorMessage(message){
+      emailError.textContent = message;
+      emailInput.classList.add("error")
+
+  }
+
+  function clearErrorMessage(){
+      emailError.textContent = "";
+      emailInput.classList.remove("error");
+  }
+
+
+});
